a80f7b1f0223c1241840759022c76f063d72899a
Erik <erikjohansson890@gmail.com>
2020-12-14 13:22:25 +0100

    almost done

--- volvo-cars/src/components/cars.js
+++ volvo-cars/src/components/cars.js
@@ -4,57 +4,86 @@ import { A } from 'hookrouter';
 //CSS
 import './styles/stylesCars.css';
 
-import Filter from './Filter'
-
 //imgs
 import blueArrow from '../docs/chevron-small.svg'
 
 function Cars() {
     const [carsList, setCarsList] = useState([]);
+    const [bodyTypes, setBodyTypes] = useState([]);
+    const [allCars, setAllCars] = useState([]);
+    const [showBackBtn, setShowBackBtn] = useState(true);
     
     
     useEffect( () => { 
        fetch('api/cars.json')
             .then((res) => res.json())
             .then((data) => {
-                //console.log('data:', data);
                 setCarsList(data)
-                   
+                setAllCars(data)
         })
-        
+    
     },[]) //useEffect 1 
+
+    useEffect( () => { 
+        let list = []
+        carsList.forEach(car => list.push(car.bodyType))
+        let bodies = [...new Set(list)]
+        setBodyTypes(bodies)
+     
+     },[carsList]) //useEffect 2
+     
+     function sortByBodyType(body) {
+        let result = carsList.filter(cars =>  { return cars.bodyType.includes(body) });
+        setCarsList(result)
+        setShowBackBtn(!showBackBtn)
+     } //sortByBodyType
+
+     function goBack() {
+        setCarsList(allCars)
+        setShowBackBtn(!showBackBtn)
+    } //goBack
     
  return (
-        <div className="cars-page">
-             <Filter cars={carsList} />
-            <div className="div-cars">
-                { carsList.map((car, index) => { 
-                    return <li key={index}>
-                                <div className="car-info-div-1">
-                                    <span className="bodyType">{car.bodyType.toUpperCase()}</span>
-                                        <div className="car-info-div-2" >
-                                            <span>{car.modelName}</span>
-                                            <span className="car-model-type">{car.modelType}</span>
-                                        </div>
-                                </div> 
-                                    <img src={car.imageUrl} alt="failed to load" />
+        <div className="filter-body-div">
+            <span>Filter by Bodytype</span>
+                <div>
+                    { bodyTypes.map((body, index) => {
+                        return <li key={index}>
+                                    <div>
+                                        <span className="btn-body-type" onClick={() => sortByBodyType(body)}>{body.toUpperCase()}</span>
+                                    </div>
+                                </li>
+                    })}
+                    { !showBackBtn && <span className="btn-back" onClick={() => goBack()}>Back</span> }
+                </div>
+        
+        <div className="div-cars">
+            { carsList.map((car, index) => { 
+                return <li key={index}>
+                            <div className="car-info-div-1">
+                                <span className="bodyType">{car.bodyType.toUpperCase()}</span>
+                                    <div className="car-info-div-2" >
+                                        <span>{car.modelName}</span>
+                                        <span className="car-model-type">{car.modelType}</span>
+                                    </div>
+                            </div> 
+                                <img src={car.imageUrl} alt="failed to load" />
                                         <div className="btn-div">
-                                                <A href={`/Learn/${car.id}`}>
-                                                    <span>LEARN</span>
-                                                    <img className="blue-arrows" src={blueArrow} alt="failed to load"/> 
-                                                </A> 
-                                                <A href={`/Shop/${car.id}`}>
+                                            <A href={`/Learn/${car.id}`}>
+                                                <span>LEARN</span>
+                                                <img className="blue-arrows" src={blueArrow} alt="failed to load"/> 
+                                            </A> 
+                                            <A href={`/Shop/${car.id}`}>
                                                     <span>SHOP</span>
                                                     <img className="blue-arrows" src={blueArrow} alt="failed to load"/> 
-                                                </A>
+                                            </A>
                                         </div>
-                            </li>
+                        </li>
                     })
                 }
             </div> 
         </div> //cars-page
     );
 }
-
 
 export default Cars;
